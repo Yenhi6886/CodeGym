2758351f41cc7fe649401af8a5628b37668254ca
Yenhi6886 <nguyenyenhi220904@gmail.com>
2025-06-18 14:40:22 +0700

    add

--- Module3/Bai2/quanlybanhang.sql
+++ Module3/Bai2/quanlybanhang.sql
@@ -1,20 +1,35 @@
-create database `quanlybanhang`;
-use `quanlybanhang`;
+CREATE DATABASE QuanLyBanHang;
+USE QuanLyBanHang;
 
+-- Bảng Khách hàng
+CREATE TABLE Customer (
+    cID INT PRIMARY KEY AUTO_INCREMENT,
+    cName VARCHAR(100),
+    cAge INT
+);
+
+-- Bảng Sản phẩm
+CREATE TABLE Product (
+    pID INT PRIMARY KEY AUTO_INCREMENT,
+    pName VARCHAR(100),
+    pPrice DECIMAL(10,2)
+);
+
+-- Bảng Đơn hàng
 CREATE TABLE `Order` (
     oID INT PRIMARY KEY AUTO_INCREMENT,
     cID INT,
     oDate DATE,
-    oTotalPrice DECIMAL(10 , 2 ),
-    FOREIGN KEY (cID)
-        REFERENCES Customer (cID)
+    oTotalPrice DECIMAL(10,2),
+    FOREIGN KEY (cID) REFERENCES Customer(cID)
 );
 
-create table OrderDetail(
-oID int,
-pID int,
-odQTY int,
-primary key(oID,pID),
-foreign key(oID) references `Order` (oID),
-foreign key(pID) references Product (pID)
+-- Bảng Chi tiết hóa đơn (trung gian)
+CREATE TABLE OrderDetail (
+    oID INT,
+    pID INT,
+    odQTY INT,
+    PRIMARY KEY (oID, pID),
+    FOREIGN KEY (oID) REFERENCES `Order`(oID),
+    FOREIGN KEY (pID) REFERENCES Product(pID)
 );
